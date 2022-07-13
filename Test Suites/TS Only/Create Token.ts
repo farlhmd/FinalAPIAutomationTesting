752231ed36421ea556851421b7fa22c2c7ca7f6c
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-07-13 16:53:28 +0700

    Create Create Token.ts

---
+++ Test Suites/TS Only/Create Token.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Create Token</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7ee6471b-7fee-4f9d-b8e3-27c5943667a8</testSuiteGuid>
+   <testCaseLink>
+      <guid>fdf702bf-07ee-426a-86f5-7b4628b7d7af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Auth/CreateToken</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
